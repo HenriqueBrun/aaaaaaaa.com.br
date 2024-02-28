0ed98dba668accf7caaf58211de33a5deaa82cb8
Henrique Brun <riquebcpires@hotmail.com>
2024-02-27 21:48:58 -0300

    new vue app

---
+++ src/components/AAAAAAAAAHome.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+import AAAAAAAAInput from './AAAAAAAAInput.vue'
+
+const aaaaaaaa = ref('AAAAAAAA')
+</script>
+
+<template>
+  <AAAAAAAAInput v-model="aaaaaaaa">
+    <template #heading>AAAAAAAA</template>
+    <template #placeholder>AAAAAAAA</template>
+  </AAAAAAAAInput>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AAAAAAAAAHome from '../components/AAAAAAAAAHome.vue'
+</script>
+
+<template>
+  <main>
+    <AAAAAAAAAHome />
+  </main>
+</template>
