594e6c7f70b4b44e4dd00fda6ce2933a38f4242a
Henrique Brun Castro Pires <riquebcpires@hotmail.com>
2025-02-24 16:24:14 -0300

    Update AAAAAAAAAFooter.vue

--- src/components/AAAAAAAAAFooter.vue
+++ src/components/AAAAAAAAAFooter.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="footer">
-        <p class="footer-text">&#128679; Work MAYBE in Progress, might as well be left behind &#128679;</p>
+        <p class="footer-text">&#128679; Work in Progress or might as well be left behind &#128679;</p>
     </div>
 </template>
 
