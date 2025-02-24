fadd264b6f4c943a3065856d8c1958ccf26130e8
Henrique Brun Castro Pires <riquebcpires@hotmail.com>
2025-02-24 16:06:04 -0300

    Update HomeView.vue with instagram free widget

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,14 @@ import AAAAAAAAAFooter from '../components/AAAAAAAAAFooter.vue'
       </div>
       <div class="img">
         <img class="marsh" src="../assets/imgs/AAAAAAAAMarsh.jpeg">
+        <!-- LightWidget WIDGET -->
+        <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+        <iframe src="//lightwidget.com/widgets/28ec89879ef6532493e13a390d96b960.html"
+                scrolling="no" 
+                allowtransparency="true" 
+                class="lightwidget-widget" 
+                style="width:100%;border:0;overflow:hidden;">
+        </iframe>
       </div>
     </div>
   </main>
